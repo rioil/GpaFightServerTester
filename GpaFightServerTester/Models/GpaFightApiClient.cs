80d9041bcf7a795dbba7d637805f44a610e750f7
rioil <rioil.dev@gmail.com>
2022-09-09 17:19:39 +0900

    指定ユーザーの作成・ログイン・削除機能を追加

--- GpaFightServerTester/Models/GpaFightApiClient.cs
+++ GpaFightServerTester/Models/GpaFightApiClient.cs
@@ -16,6 +16,8 @@ namespace GpaFightServerTester.Models
 
         public string? Token { get; set; }
 
+        public User? CurrentUser { get; set; }
+
         public async Task<User?> CreateUser(User user)
         {
             var result = await CreateUserInternal(user);
@@ -23,11 +25,10 @@ namespace GpaFightServerTester.Models
             return result.IsSuccessful ? result.Data : null;
         }
 
-        public async Task<string?> CreateUserRaw(User user)
+        public async Task<RestResponse?> CreateUserRaw(User user)
         {
             var result = await CreateUserInternal(user);
-
-            return result.Content;
+            return result;
         }
 
         public async Task<User?> GetUser(string id)
@@ -37,11 +38,11 @@ namespace GpaFightServerTester.Models
             return result.IsSuccessful ? result.Data : null;
         }
 
-        public async Task<string?> GetUserRaw(string id)
+        public async Task<RestResponse?> GetUserRaw(string id)
         {
             var result = await GetUserInternal(id);
 
-            return result.Content;
+            return result;
         }
 
         public async Task<bool> UpdateUser(User user)
@@ -51,11 +52,35 @@ namespace GpaFightServerTester.Models
             return result.IsSuccessful;
         }
 
-        public async Task<string?> UpdateUserRaw(User user)
+        public async Task<RestResponse?> UpdateUserRaw(User user)
         {
             var result = await UpdateUserInternal(user);
 
-            return result.Content;
+            return result;
+        }
+
+        public async Task<bool> DeleteUser(User user)
+        {
+            var result = await DeleteUserInternal(user);
+
+            return result.IsSuccessful;
+        }
+
+        public async Task<RestResponse?> DeleteUserRaw(User user)
+        {
+            return await DeleteUserInternal(user);
+        }
+
+        public async Task<bool> GetCurrentUser()
+        {
+            await GetCurrentUserInternal();
+
+            return CurrentUser is not null;
+        }
+
+        public async Task<RestResponse<User>?> GetCurrentUserRaw()
+        {
+            return await GetCurrentUserInternal();
         }
 
         public async Task<bool> Login(LoginCredential loginCredential)
@@ -91,7 +116,7 @@ namespace GpaFightServerTester.Models
         public async Task<Ranking?> GetWholeRanking()
         {
             var result = await GetWholeRankingInternal();
-            
+
             return result.IsSuccessful ? result.Data : null;
         }
 
@@ -140,11 +165,38 @@ namespace GpaFightServerTester.Models
 
         private async Task<RestResponse> UpdateUserInternal(User user)
         {
-            var request = new RestRequest($"users/{user.Username}");
+            if (Token is null) { throw new InvalidOperationException(); }
+
+            var request = new RestRequest($"users/{user.Id}");
+            request.AddHeader("Authorization", $"Bearer {Token}");
             var serializedBody = JsonSerializer.Serialize(user, _serializerOptions);
             request.AddJsonBody(serializedBody);
 
             var result = await _restClient.ExecutePostAsync(request);
+
+            return result;
+        }
+
+        private async Task<RestResponse> DeleteUserInternal(User user)
+        {
+            if (Token is null) { throw new InvalidOperationException(); }
+
+            var request = new RestRequest($"users/{user.Id}", Method.Delete);
+            request.AddHeader("Authorization", $"Bearer {Token}");
+            var result = await _restClient.ExecuteAsync(request);
+
+            return result;
+        }
+
+        private async Task<RestResponse<User>?> GetCurrentUserInternal()
+        {
+            if (Token is null) { throw new InvalidOperationException(); }
+
+            var request = new RestRequest("users/current");
+            request.AddHeader("Authorization", $"Bearer {Token}");
+            var result = await _restClient.ExecuteGetAsync<User>(request);
+
+            CurrentUser = result?.Data;
 
             return result;
         }
--- GpaFightServerTester/ViewModels/MainWindowViewModel.cs
+++ GpaFightServerTester/ViewModels/MainWindowViewModel.cs
@@ -10,7 +10,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Printing;
 using System.Text;
+using System.Text.Json;
 
 namespace GpaFightServerTester.ViewModels
 {
@@ -21,12 +23,34 @@ namespace GpaFightServerTester.ViewModels
         {
         }
 
+        private readonly GpaFightApiClient _client = new GpaFightApiClient();
+
         public ObservableCollection<string> Logs
         {
             get => _logs ??= new();
             set => RaisePropertyChangedIfSet(ref _logs, value);
         }
         private ObservableCollection<string>? _logs;
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        public string Username
+        {
+            get => _username;
+            set => RaisePropertyChangedIfSet(ref _username, value);
+        }
+        private string _username = string.Empty;
+
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        public string Password
+        {
+            get => _password;
+            set => RaisePropertyChangedIfSet(ref _password, value);
+        }
+        private string _password = string.Empty;
 
         public async void RunTest()
         {
@@ -58,12 +82,12 @@ namespace GpaFightServerTester.ViewModels
                 }
                 AddLog("ログイン成功");
 
-                AddLog(await client.GetUserRaw("current"));
+                AddLog((await client.GetUserRaw("current"))?.Content);
 
                 if (createdUser?.Id is not null)
                 {
                     var userInfo = await client.GetUserRaw(createdUser.Id);
-                    AddLog(userInfo);
+                    AddLog(userInfo?.Content);
                 }
 
                 if (!await client.UpdateGrade(new(gpas[random.Next() % gpas.Length])))
@@ -104,6 +128,79 @@ namespace GpaFightServerTester.ViewModels
         private ViewModelCommand? _RunTestCommand;
         public ViewModelCommand RunTestCommand => _RunTestCommand ??= new ViewModelCommand(RunTest);
 
+        /// <summary>
+        /// ユーザー作成
+        /// </summary>
+        public async void CreateUser()
+        {
+            var response = await _client.CreateUserRaw(new(Username, 2019, "基礎工学部", "情報科学科", Password));
+            if (response?.IsSuccessful ?? false)
+            {
+                AddLog("ユーザー作成成功");
+            }
+            else
+            {
+                AddLog("ユーザー作成失敗");
+            }
+
+            AddLog(response?.Content);
+        }
+        private ViewModelCommand? _createUserCommand;
+        public ViewModelCommand CreateUserCommand => _createUserCommand ??= new ViewModelCommand(CreateUser);
+
+        /// <summary>
+        /// ログイン
+        /// </summary>
+        public async void Login()
+        {
+            if (await _client.Login(new(Username, Password)) && await _client.GetCurrentUser())
+            {
+                AddLog("ログイン成功");
+            }
+            else
+            {
+                AddLog("ログイン失敗");
+            }
+        }
+        private ViewModelCommand? _loginCommand;
+        public ViewModelCommand LoginCommand => _loginCommand ??= new ViewModelCommand(Login);
+
+        /// <summary>
+        /// ユーザー削除
+        /// </summary>
+        public async void DeleteUser()
+        {
+            if (_client.CurrentUser is null)
+            {
+                AddLog("未ログイン");
+                return;
+            }
+
+            if (await _client.DeleteUser(_client.CurrentUser))
+            {
+                AddLog("ユーザー削除成功");
+            }
+            else
+            {
+                AddLog("ユーザー削除失敗");
+            }
+        }
+        private ViewModelCommand? _deleteUserCommand;
+        public ViewModelCommand DeleteUserCommand => _deleteUserCommand ??= new ViewModelCommand(DeleteUser);
+
+        public async void UpdateGrade(string gpa)
+        {
+            if (await _client.UpdateGrade(new(gpa)))
+            {
+                AddLog("成績更新成功");
+            }
+            else
+            {
+                AddLog("成績更新失敗");
+            }
+        }
+        private ListenerCommand<string>? _updateGradeCommand;
+        public ListenerCommand<string> UpdateGradeCommand => _updateGradeCommand ??= new ListenerCommand<string>(UpdateGrade);
 
         private void AddLog(string? message)
         {
